d978eead6056fa4259f225b659be53fe32ccc008
Christopher Kumm <chrislkumm@protonmail.com>
2022-07-30 13:07:06 -0400

    Get official example working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'modern-normalize'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,49 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import type { NextPage } from 'next'
+import { Mesh } from 'three'
+
+type BoxProps = {
+  position: [number, number, number]
+}
+
+function Box(props: BoxProps) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<Mesh>(null)
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame(() => {
+    if (!mesh.current) return
+    mesh.current.rotation.x += 0.01
+  })
+
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 const Home: NextPage = () => {
-  return <>TODO: Make something</>
+  return (
+    <Canvas style={{ height: '100vh', width: '100%' }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
 }
 
 export default Home
