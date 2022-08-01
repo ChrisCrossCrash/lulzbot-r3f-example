eebb45b5d0a5f2a411805442c55d8ce1193291f5
Christopher Kumm <chrislkumm@protonmail.com>
2022-07-31 22:16:59 -0400

    Improve layout on mobile and tablet devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
 import { Canvas } from '@react-three/fiber'
-import { PresentationControls } from '@react-three/drei'
+import { PresentationControls, Bounds } from '@react-three/drei'
 import Balljoint from '../components/Balljoint'
 import type { NextPage } from 'next'
 import { Container, Row, Col } from 'react-bootstrap'
 import styles from './index.module.scss'
 
 const Home: NextPage = () => {
+  /** The transition breakpoint where the section converts from a col to a row. */
+  // const transitionSize: ContainerProps['fluid'] = 'md'
+
   return (
-    <Container>
+    <Container fluid={'md'}>
       <Row className={styles.samplePrintRow}>
-        <Col lg={4} md={6} xs={12} className={styles.infoCol}>
+        <Col className={styles.canvasContainer}>
+          <Canvas camera={{ position: [0, 0.2, 1.5], fov: 30 }}>
+            <Bounds fit observe>
+              <PresentationControls global cursor polar={[-0.15, 0.75]}>
+                <Balljoint position={[0, -0.2, 0]} />
+              </PresentationControls>
+            </Bounds>
+          </Canvas>
+        </Col>
+        <Col
+          lg={{ span: 4, order: 'first' }}
+          md={{ span: 6, order: 'first' }}
+          span={12}
+          className={styles.infoCol}
+        >
           <h1>Get a Free Sample 3D Print</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
@@ -17,13 +34,6 @@ const Home: NextPage = () => {
             quaerat doloribus quia.
           </p>
           <button>Order Now</button>
-        </Col>
-        <Col className={styles.canvasContainer}>
-          <Canvas camera={{ position: [0, 0.2, 0.8], fov: 60 }}>
-            <PresentationControls global cursor polar={[-0.45, 0.75]}>
-              <Balljoint position={[0, -0.2, 0]} />
-            </PresentationControls>
-          </Canvas>
         </Col>
       </Row>
     </Container>
